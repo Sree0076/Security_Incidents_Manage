c8a1bf45bd09d49c7dac2ddd778c6e0642b54fac
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2024-08-23 11:49:05 +0530

    updated view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { UserDasboardComponent } from './pages/user-dasboard/user-dasboard.compo
 import { IncidentCreateFormComponentComponent } from './components/incident-create-form-component/incident-create-form-component.component';
 
 import { NotificationComponentComponent } from './components/notification-component/notification-component.component';
+import { FirstLoginContentComponent } from './components/first-login-content/first-login-content.component';
+import { ViewIncidentFormComponent } from './components/view-incident-form/view-incident-form.component';
 
 export const appRoutes: Route[] = [
-
-    { path: '', component: LoginPageComponent },
-    { path: 'admin', component: AdminDasboardComponent },
-    { path: 'user', component: UserDasboardComponent },
-    { path: 'form', component: IncidentCreateFormComponentComponent },
-    { path: 'noti', component: NotificationComponentComponent },
+  { path: '', component: LoginPageComponent },
+  { path: 'admin', component: AdminDasboardComponent },
+  { path: 'user', component: UserDasboardComponent },
+  { path: 'form', component: IncidentCreateFormComponentComponent },
+  { path: 'noti', component: NotificationComponentComponent },
+  { path: 'initial-page', component: FirstLoginContentComponent },
+  { path: 'view-incident', component: ViewIncidentFormComponent },
 ];
