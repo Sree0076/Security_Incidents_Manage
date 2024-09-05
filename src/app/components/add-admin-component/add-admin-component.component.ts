d621a8aacb0cc533365dbe60069ca1fd8f1c9742
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2024-09-05 12:34:30 +0530

    changed add admin modaal

--- src/app/components/add-admin-component/add-admin-component.component.ts
+++ src/app/components/add-admin-component/add-admin-component.component.ts
@@ -10,13 +10,31 @@ import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
-import { ForwardPipePipe } from "../../pipes/forward/forward-pipe.pipe";
+import { ForwardPipePipe } from '../../pipes/forward/forward-pipe.pipe';
 import { VariablesSharedService } from 'src/app/services/shared/sharedVariables/variables.shared.service';
+import { FilterPipe } from 'src/app/pipes/addadmin/filter.pipe';
 
+interface userDetails {
+  id: number;
+  user_icon: string;
+  name: string;
+  department: string;
+  designation: string;
+  email: string;
+}
 @Component({
   selector: 'app-add-admin-component',
   standalone: true,
-  imports: [CommonModule, FormsModule, DialogModule, ButtonModule, InputTextModule, CheckboxModule, ForwardPipePipe],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    CheckboxModule,
+    ForwardPipePipe,
+    FilterPipe,
+  ],
   templateUrl: './add-admin-component.component.html',
   styleUrl: './add-admin-component.component.css',
 })
@@ -28,72 +46,68 @@ export class AddAdminComponentComponent implements OnInit {
     '595px': '90vw',
     '500px': '95vw',
     '460px': '99vw',
-    '380px': '99vw'
+    '380px': '99vw',
   };
-  @Input() visibility=false;
-  forwardIncidentId =0;
-  user_details:any[]=[];
-  searchTerm='';
+  @Input() visibility = false;
+  forwardIncidentId = 0;
+  user_details: any[] = [];
+  searchTerm = '';
   selectedUsers: any[] = [];
-  selectedUsersId:number [] =[];
-  message='';
+  selectedUsersId: number[] = [];
+  message = '';
   checkboxes: { [key: string]: boolean } = {
-    'incidentManagement': false,
-    'adminManagement': false
+    incidentManagement: false,
+    adminManagement: false,
   };
-  constructor(public forwardFormService : ForwardServiceService,
+  constructor(
+    public forwardFormService: ForwardServiceService,
     private usermanagement: EmployeeServiceService,
-    private addadminmodalService: VariablesSharedService,
-    
-      ){}
-  isForwardform=false;
-  ngOnInit():void{
-
-    this.addadminmodalService.addAdminModalVisible$.subscribe(visible => {
+    private addadminmodalService: VariablesSharedService
+  ) {}
+  isForwardform = false;
+  ngOnInit(): void {
+    this.addadminmodalService.addAdminModalVisible$.subscribe((visible) => {
       this.visibility = visible;
     });
 
-    this.forwardFormService.getAllUsers().subscribe(data =>
-      {
-        this.user_details = data;
-        console.log(data);
-
-      }
-      )
+    this.forwardFormService.getAllUsers().subscribe((data) => {
+      this.user_details = data;
+      console.log(data);
+    });
   }
-    addUser(user: any) {
-      if (!this.selectedUsers.find(u => u.id === user.id)) {
-        this.selectedUsers.push(user);
-      }
+  addUser(user: any) {
+    if (!this.selectedUsers.find((u) => u.id === user.id)) {
+      this.selectedUsers.push(user);
     }
-    // Remove user from the selected list
-    removeUser(user: any) {
-      this.selectedUsers = this.selectedUsers.filter(u => u.id !== user.id);
-    }
-    getSelectedUserIds(): number[] {
-      return this.selectedUsers.map(user => user.id);
-
-    }
-    add(){
-      if(this.selectedUsers)
-      {
+  }
+  // Remove user from the selected list
+  removeUser(user: any) {
+    this.selectedUsers = this.selectedUsers.filter((u) => u.id !== user.id);
+  }
+  getSelectedUserIds(): number[] {
+    return this.selectedUsers.map((user) => user.id);
+  }
+  add() {
+    if (this.selectedUsers) {
       const data = {
-        "employeeId": this.selectedUsers[0].id,
-        "assignedBy": 2,
-        "isIncidentMangenet": this.checkboxes['incidentManagement'],
-        "isUserMangenet": this.checkboxes['adminManagement'],
-        "status": true,
+        employeeId: this.selectedUsers[0].id,
+        assignedBy: 2,
+        isIncidentMangenet: this.checkboxes['incidentManagement'],
+        isUserMangenet: this.checkboxes['adminManagement'],
+        status: true,
       };
+      console.log(data);
+      
       this.usermanagement.createUser(data).subscribe((response) => {
         console.log('Incident added successfully', response);
-      this.resetForm();
-    });
-
+        this.resetForm();
+      });
     }
   }
-    resetForm(): void {
-      this.selectedUsers = [];
-      this.searchTerm = '';
-      this.message = '';
-    }
+  resetForm(): void {
+    this.selectedUsers = [];
+    this.searchTerm = '';
+    this.message = '';
+  }
+  
 }
